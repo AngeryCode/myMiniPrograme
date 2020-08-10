3f99b2e138fd9405b6d17995989e809fe9b1d816
nf.wu <nf.wu@izhaohu.com>
2020-08-10 10:19:34 +0800

    add goodslist pulldownrefresh and all page request loading

--- request/myRequest.js
+++ request/myRequest.js
@@ -1,4 +1,10 @@
+let requestTimes = 0;
 export const myRequest = (params) => {
+    requestTimes++;
+    wx.showLoading({
+        title: '加载中...',
+        mask: true,
+    });
     const baseUrl = 'https://api-hmugo-web.itheima.net/api/public/v1'
     return new Promise ((resolve, reject) => {
         wx.request({
@@ -9,6 +15,12 @@ export const myRequest = (params) => {
             },
             fail: (e)=>{
                 reject(e)
+            },
+            complete: ()=> {
+                requestTimes--;
+                if (requestTimes === 0) {
+                    wx.hideLoading();
+                }
             }
         });
     })
